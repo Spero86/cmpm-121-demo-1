0bd47ea9f42af3f52a47702f3fdd35526f9de2b2
Patrick James Alcantara <151216151+Spero86@users.noreply.github.com>
2024-10-16 01:35:00 -0700

    Checking if button appears

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Guitar Clicker";
+const gameName = "Guitar Strummer";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Step 1 - A button you can click
+const button = document.createElement("button");
+button.textContent = "🎸";
