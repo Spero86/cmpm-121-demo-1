f7be6fa9e2e6c3601b023e480fde8cf19bea2edc
Patrick James Alcantara <151216151+Spero86@users.noreply.github.com>
2024-10-16 01:44:16 -0700

    Check if button is working

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,18 @@ app.append(header);
 // Step 1 - A button you can click
 const button = document.createElement("button");
 button.textContent = "🎸";
+
+let guitarStrum = 0; // keeps track of how many times the guitar has been strummed
+
+
+button.addEventListener('click', () => {
+    guitarStrum++;
+});
+
+// button formatting
+button.style.padding = '15px 15px';
+button.style.fontSize = '35px';
+button.style.marginTop = '10px';
+
+console.log(guitarStrum); // states how many times the guitar has been strummed
+app.append(button); // adds button
